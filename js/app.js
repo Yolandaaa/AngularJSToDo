37cedd1a682791760bbf54c38f194247a0106575
Travo100 <Travo100@gmail.com>
2015-05-21 07:53:21 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+angular.module('todo', ['ui.bootstrap'])
+.controller('listIt', function(){
+  var vm = this;
+  vm.items = [];  
+  
+  vm.addItem = function() {
+    if(vm.item === undefined || vm.item === "") {
+      vm.noItem = "Please enter a todo item."
+    } else {
+      vm.items.push({
+        text : vm.item,
+        editing : false,
+        done : false
+      });
+      vm.items.reverse();
+      vm.updateUI();
+    }
+  };
+  
+  vm.removeItem = function(index) {
+    vm.items.splice(index, 1);
+    vm.updateUI();
+  };
+
+  vm.editItem = function(index) {
+    vm.items[index].editing = !vm.items[index].editing;
+  };
+
+  vm.updateUI = function(){
+    
+    vm.item = "";
+    vm.noItem = "";
+  };
+
+  vm.remaining = function() {
+    var itemsLeft = 0;
+    angular.forEach(vm.items, function(item) {
+      itemsLeft += item.isDone ? 0 : 1;
+    });
+    return itemsLeft;
+  };
+
+  // todoList.remaining = function() {
+  //     var count = 0;
+  //     angular.forEach(todoList.todos, function(todo) {
+  //       count += todo.done ? 0 : 1;
+  //     });
+  //     return count;
+  //   };
+
+
+
+    
+
+});
